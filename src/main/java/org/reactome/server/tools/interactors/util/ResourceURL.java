d99a1f5401c70e269f65bc858255cc389ef74b10
gviteri <gviteri@ebi.ac.uk>
2016-02-15 16:01:27 +0000

    Remove urls for the services we can't provide it

--- src/main/java/org/reactome/server/tools/interactors/util/ResourceURL.java
+++ src/main/java/org/reactome/server/tools/interactors/util/ResourceURL.java
@@ -7,9 +7,26 @@ package org.reactome.server.tools.interactors.util;
 @SuppressWarnings("unused")
 public enum ResourceURL {
 
-    DIP("http://identifiers/dip/##ID##", null, null),
+    APID(InteractorConstant.DEFAULT_PROTEIN_URL, null, null),
     BIOGRID("http://identifiers/biogrid/##ID##", null, null),
     BIND("http://identifiers/bind/##ID##", null, null),
+    BINDINGDB("http://identifiers.org/bindingDB/##ID##", null, null),
+    CHEMBL(null, null, null),
+    //CHEMBL(InteractorConstant.DEFAULT_PROTEIN_URL, "https://www.ebi.ac.uk/chembldb/index.php/compound/inspect/##ID##", "https://www.ebi.ac.uk/chembldb/index.php/assay/inspect/##ID##"),
+    DIP("http://identifiers/dip/##ID##", null, null),
+    DRUGBANK(null, null, null),
+    INNATEDB(null, null, null),
+    INNATEDBALL(null, null, null),
+    IREFINDEX(null, null, null),
+    INTEROPORC(null, null, null),
+    MATRIXDB(null, null, null),
+    MENTHA(null, null, null), // tricky
+    REACTOME(null, null, null),
+    REACTOMEFIS(null, null, null),
+    GENEMANIA(null, null, null),// tricky
+    BAR(null, null, null),
+    EBIGOANONINTACT(null, null, null),
+    VIRHOSTNET(null, null, null),
     DEFAULT(InteractorConstant.DEFAULT_PROTEIN_URL, InteractorConstant.DEFAULT_CHEMICAL_URL, InteractorConstant.DEFAULT_INTERACTION_URL);
 
     private String protein;
@@ -46,11 +63,11 @@ public enum ResourceURL {
         this.interaction = interaction;
     }
 
-    public static ResourceURL getByName(String name){
+    public static ResourceURL getByName(String name) {
         if (name == null || name.isEmpty()) return null;
-        String term = name.toUpperCase();
+        String term = name.toUpperCase().replaceAll("-", "");
         for (ResourceURL resourceURL : values()) {
-            if(resourceURL.name().toUpperCase().equals(term)) return resourceURL;
+            if (resourceURL.name().toUpperCase().equals(term)) return resourceURL;
         }
         return DEFAULT;
     }
