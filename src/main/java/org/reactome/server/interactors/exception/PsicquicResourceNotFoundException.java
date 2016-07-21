ddc0a31d01e5adf1312cb8b6658c55d9d5f9da6c
gviteri <gviteri@ebi.ac.uk>
2016-07-21 13:29:54 +0100

    Update comments

--- src/main/java/org/reactome/server/interactors/exception/PsicquicResourceNotFoundException.java
+++ src/main/java/org/reactome/server/interactors/exception/PsicquicResourceNotFoundException.java
@@ -2,8 +2,9 @@ package org.reactome.server.interactors.exception;
 
 /**
  * Exception thrown when trying to Query PSICQUIC
- * PSICQUIC Registry shows the service is up and running
- * but we can't query this particular server.
+ * using an invalid resource name.
+ * In the PWB we use the correct list, but in the content service
+ * it can be any value.
  *
  * @author Guilherme S Viteri <gviteri@ebi.ac.uk>
  */
