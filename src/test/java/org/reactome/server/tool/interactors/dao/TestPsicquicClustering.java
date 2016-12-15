91e376e21eab3ee7a1e4ee0f86a072566a7b3d0b
gviteri <gviteri@ebi.ac.uk>
2016-12-15 12:41:49 +0000

    correct APID testcase

--- src/test/java/org/reactome/server/tool/interactors/dao/TestPsicquicClustering.java
+++ src/test/java/org/reactome/server/tool/interactors/dao/TestPsicquicClustering.java
@@ -47,12 +47,11 @@ public class TestPsicquicClustering {
             // Psicquic is down, but we don't want to break our tests because of it.
             logger.warn("Couldn't get PSICQUIC Resources. Reason: PSICQUIC is down.");
         }
-
     }
 
     @Test
     public void testSpecificPsicquicResource() {
-        String resourceName = "HPIDb";
+        String resourceName = "UniProt";
 
         try {
             //long start = System.currentTimeMillis();
@@ -175,144 +174,111 @@ public class TestPsicquicClustering {
     private Collection<String> getSampleAccessions(String resource) {
         Collection<String> accessions = new ArrayList<>(1);
         switch (resource) {
-            case "APID":
+            case "APID Interactomes":
                 accessions.add("P00533");
                 break;
-
             case "BAR":
                 accessions.add("Q9LFH5");
                 accessions.add("Q7PC79");
                 break;
-
             case "BioGrid":
                 accessions.add("29101");
                 break;
-
             case "BIND":
                 accessions.add("16761");
                 break;
-
             case "BindingDB":
                 accessions.add("O43353");
                 break;
-
             case "bhf-ucl":
                 accessions.add("P35222");
                 break;
-
             case "ChEMBL":
                 accessions.add("Q02750");
                 accessions.add("161511");
                 break;
-
             case "DIP":
                 accessions.add("DIP-232N");
                 accessions.add("Q8IHE3");
                 break;
-
             case "DrugBank":
                 accessions.add(""); // DOWN
                 break;
-
             case "EBI-GOA-nonIntAct":
                 accessions.add("Q13485");
                 accessions.add("P84022");
                 break;
-
             case "GeneMANIA":
                 accessions.add("Q13501"); // This is impossible (better if we remove it ... we should report this error in the score
                 break;
-
             case "HPIDb":
                 accessions.add("Q32PH0");
                 accessions.add("K7Y1A2");
                 break;
-
             case "InnateDB":
                 accessions.add("Q9BXM7");
                 break;
-
             case "InnateDB-All":
                 accessions.add("Q9BXM7");
                 break;
-
             case "IntAct":
                 accessions.add("Q13501");
                 break;
-
             case "iRefIndex":
                 accessions.add(""); // DOWN
                 break;
-
             case "Interoporc":
                 accessions.add(""); // does not have score, can't find any higher than 0.45. Very old DB.
                 break;
-
             case "I2D":
                 accessions.add("P00533"); // does not have score, can't find any higher than 0.45.
                 break;
-
             case "I2D-IMEx":
                 accessions.add("P12830");
                 break;
-
             case "InnateDB-IMEx":
                 accessions.add("Q8N7N6");
                 break;
-
             case "MatrixDB":
                 accessions.add("P13213");
                 break;
-
             case "mentha":
                 //accessions.add("Q9NWZ3");
                 accessions.add("Q15262");
                 break;
-
             case "MPIDB":
                 accessions.add("Q70M91");
                 break;
-
             case "MBInfo":
                 accessions.add("P07228");
                 break;
-
             case "MINT":
                 accessions.add("O60231");
                 break;
-
             case "MolCon":
                 accessions.add("Q92917");
                 break;
-
             case "Reactome":
                 accessions.add("Q99661"); // does not have score, can't find any higher than 0.45.
                 break;
-
             case "Reactome-FIs":
                 accessions.add("O75534");
                 break;
-
             case "STRING":
                 accessions.add(""); // DOWN
                 break;
-
             case "Spike":
                 accessions.add("P05412");
                 break;
-
             case "TopFind":
                 accessions.add(""); // DOWN
                 break;
-
             case "UniProt":
                 accessions.add("Q8WV24");
                 break;
-
             case "VirHostNet":
                 accessions.add("Q01844"); // does not have score, can't find any higher than 0.45.
                 break;
-
             case "ZINC":
                 accessions.add("Q99720");
                 break;
